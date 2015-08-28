d76fb6eef2cd12855db3710a3424adcf8d3bc0fb
Joseph P DeWitt <josephdewitt8@gmail.com>
2015-08-28 02:30:18 +0000

    first FT spected out

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,30 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://josephdewitt8.pythonanywhere.com')
+class NewVisitorTest( unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly.wait(3)
 
-assert "Django" in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # online to-do list
+        self.browser.get('http://josephdewitt8.pythonanywhere.com')
+        # the pape title and header mention to-to list
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+# she is invited to enter a to-do list straight away
+# she type "buy Something" into a text box when she hits enter
+# the page updates, and now the page lists
+# "1. buy something"
+# there is still a text box inviting her to add another. She
+# enters " use purchased item"
+# the page updates again, and now both items are on her list
+# The site generated a unique URL for her -- there is text explaining tthe URL
+# She visits the URL - her To-Do lost is still there.
+#
+if __name__ == '__main__':
+    unittest.main()
