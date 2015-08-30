8c57e9dac735ff6757a4025c7ecec28affe64693
Joseph P DeWitt <josephdewitt8@gmail.com>
2015-08-30 22:05:32 +0000

    Functional test now checks we can input a to-do list

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest( unittest.TestCase):
@@ -12,16 +13,34 @@ class NewVisitorTest( unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # online to-do list
         self.browser.get('http://josephdewitt8.pythonanywhere.com')
+
         # the pape title and header mention to-to list
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-# she is invited to enter a to-do list straight away
-# she type "buy Something" into a text box when she hits enter
-# the page updates, and now the page lists
-# "1. buy something"
-# there is still a text box inviting her to add another. She
-# enters " use purchased item"
+
+        # she is invited to enter a to-do list straight away
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item'
+        )
+
+        # she type "buy Something" into a text box when she hits enter
+        input.send_keys('Buy peacock feathers')
+
+        # the page updates, and now the page lists
+        # "1. buy something"
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.broswer.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+        # there is still a text box inviting her to add another. She
+        # enters " use purchased item"
+        self.fail('Finish the test')
+
 # the page updates again, and now both items are on her list
 # The site generated a unique URL for her -- there is text explaining tthe URL
 # She visits the URL - her To-Do lost is still there.
