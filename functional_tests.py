a22d27b1b6f13ce41992bc9ac9dc4d8448c24307
Joseph P DeWitt <josephdewitt8@gmail.com>
2015-09-05 13:54:39 +0000

    make helper method for FT

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,11 @@ class NewVisitorTest( unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(rwo_text, [row.text for row in rows)
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # online to-do list
         self.browser.get('http://josephdewitt8.pythonanywhere.com')
@@ -33,11 +38,13 @@ class NewVisitorTest( unittest.TestCase):
         # "1. buy something"
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        check_for_row_in_list_table(1: Buy peacock feathers)
 
+#        table = self.browser.find_element_by_id('id_list_table')
+#        rows = table.find_elements_by_tag_name('tr')
+#
 #        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-
+#
 #        self.assertTrue(
 #            any(row.text == '1: Buy peacock feathers' for row in rows),
 #            "New to-do item did not appear in table -- its text was:\n%s" % (table.text,
@@ -54,11 +61,11 @@ class NewVisitorTest( unittest.TestCase):
 
 # the page updates again, and now both items are on her list
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        check_for_row_in_list_table('1: Buy peacock feathers')
+        check_for_row_in_list_table('2: use peacock feathers to make a fly')
 
-        self.assertIn('1. Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: use peacock feathers to make a fly' , [row.text for row in rows])
+#        self.assertIn('1. Buy peacock feathers', [row.text for row in rows])
+#        self.assertIn('2: use peacock feathers to make a fly' , [row.text for row in rows])
 
 # The site generated a unique URL for her -- there is text explaining tthe URL
 
