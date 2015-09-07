ae6d7e65dd598ca560270ada03eaf8295d28b985
Joseph P DeWitt <josephdewitt8@gmail.com>
2015-09-07 02:56:26 +0000

    make functional_tests an app, use LiveServerTestCasemake functional_tests an app, use LiveServerTestCase

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,8 +1,9 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest( unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -13,7 +14,7 @@ class NewVisitorTest( unittest.TestCase):
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(rwo_text, [row.text for row in rows)
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # online to-do list
@@ -38,7 +39,7 @@ class NewVisitorTest( unittest.TestCase):
         # "1. buy something"
         inputbox.send_keys(Keys.ENTER)
 
-        check_for_row_in_list_table(1: Buy peacock feathers)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 #        table = self.browser.find_element_by_id('id_list_table')
 #        rows = table.find_elements_by_tag_name('tr')
@@ -61,16 +62,17 @@ class NewVisitorTest( unittest.TestCase):
 
 # the page updates again, and now both items are on her list
 
-        check_for_row_in_list_table('1: Buy peacock feathers')
-        check_for_row_in_list_table('2: use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: use peacock feathers to make a fly')
 
 #        self.assertIn('1. Buy peacock feathers', [row.text for row in rows])
 #        self.assertIn('2: use peacock feathers to make a fly' , [row.text for row in rows])
 
 # The site generated a unique URL for her -- there is text explaining tthe URL
+        self.browser.get(self.live_server_url)
 
         self.fail('Finish the test')
-# She visits the URL - her To-Do lost is still there.
+# She visits the URL - her To-Do list is still there.
 #
-if __name__ == '__main__':
-    unittest.main()
+#if __name__ == '__main__':
+#    unittest.main()
