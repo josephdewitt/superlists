7473e23b31d1d20c49bdde73284d94ec790e64ae
Joseph P DeWitt <jpdewitt@jpsenior.com>
2016-01-02 11:06:55 -0600

    add gunicorn to virtualenv requirements

--- deploy_tools/fabric.py
+++ deploy_tools/fabric.py
@@ -6,7 +6,8 @@ REPO_URL = 'http://github.com/josephdewitt/superlists.git'
 
 def deploy():
 	site_folder = '/home/%s/sites/%s' % (env.user, env.host)
-	source_folder = site_folder + 'sourse'
+	source_folder = site_folder + '/source'
 	_create_directory_structure_if_necessary(site_folder)
-	
-	
+	_get_latest_source(source_folder)
+	_update_settings(source_folder, env.host)
+	_update_virtualenv(source_folder)
